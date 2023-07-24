6d3f224e8d26caf9fb3653364b5e6cb73e47c550
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-24 11:16:32 -0300

    starting app

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesTrackerApp());
+}
+
+class ExpensesTrackerApp extends StatelessWidget {
+  const ExpensesTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Expenses Tracker');
+  }
+}
