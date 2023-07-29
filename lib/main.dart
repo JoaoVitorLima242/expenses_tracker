5d4aae0c8dc6b3d5efae9c90410421499776bb2e
JoaoVitorLima242 <jlima0500@gmail.com>
2023-07-28 22:23:26 -0300

    adding dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:expenses_tracker/screens/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
+var customColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xff00b4d8),
+);
+
 void main() {
-  runApp(MaterialApp(
-    theme: ThemeData(useMaterial3: true),
-    home: const ExpensesScreen(),
-  ));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.system,
+      home: const ExpensesScreen(),
+    ),
+  );
 }
